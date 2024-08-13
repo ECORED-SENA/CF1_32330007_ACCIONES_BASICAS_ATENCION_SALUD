30855f54a353c6055757d22975c922d47d08078d
emilydev <98632409+emilydev@users.noreply.github.com>
2024-08-13 17:10:50 -0500

    se suben referencias y glosario

--- src/config/global.js
+++ src/config/global.js
@@ -112,29 +112,109 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      tema: 'Primeros auxilios y evaluación de la escena',
+      referencia:
+        'Martín Villaoslada, O. & Arenal Laza, C. (2020). Primeros auxilios. MF0272: ( ed.). Editorial Tutor Formación.',
+      tipo: 'Capítulo 1',
+      link:
+        'https://elibro-net.bdigital.sena.edu.co/es/lc/senavirtual/titulos/125938',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: 'Actitud del auxiliador y valoración de la escena',
+      referencia:
+        'Agencia Nacional de Seguridad Vial (2023). MÓDULO 1 - DEBERES PRIMER RESPONDIENTE. [Archivo de video] Youtube.',
+      tipo: 'Video',
+      link: 'https://youtu.be/4JudaYDBcho?feature=shared',
+    },
+    {
+      tema: '¿Qué debe hacer un primer auxiliador?',
+      referencia:
+        'Tejero Fernández, J. (2022). Primeros auxilios. AFDA0211: (2 ed.). IC Editorial.',
+      tipo: 'Capítulo 3',
+      link:
+        'https://elibro-net.bdigital.sena.edu.co/es/lc/senavirtual/titulos/227699',
+    },
+    {
+      tema: 'Cinemática del trauma',
+      referencia:
+        'Centro Nacional Operativo en Salud. (2015). CINEMATICA DEL TRAUMA 2. [Archivo de video] Youtube.',
+      tipo: 'Video',
+      link:
+        'https://www.youtube.com/watch?v=yO_K1wqTQ6c&ab_channel=CentroNacionalOperativoenSalud',
+    },
+    {
+      tema: 'Botiquín de primeros auxilios',
+      referencia:
+        'Facultad de Medicina UNAM (2018). Botiquín. [Archivo de video] Youtube.',
+      tipo: 'Video',
+      link:
+        'https://www.youtube.com/watch?v=t5S7zmerXD4&ab_channel=Prevenci%C3%B3nSalud',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Texto de la definición',
+      termino: 'Escena',
+      significado:
+        'ambiente en el que se produjo la lesión y que se debe valorar.',
     },
     {
-      termino: '<em>Término con extranjerismo</em>',
-      significado: 'Definición',
+      termino: 'Evacuación',
+      significado:
+        'acción tendiente a establecer una barrera (distancia) entre una fuente de riesgo y las personas amenazadas, mediante el desplazamiento de estas.',
+    },
+    {
+      termino: 'Explosión',
+      significado:
+        'liberación brusca de una gran cantidad de energía con desprendimiento de calor, luz y gases.',
+    },
+    {
+      termino: 'Incendio',
+      significado:
+        'fenómeno que se presenta cuando uno o varios materiales combustibles o inflamables son consumidos de forma incontrolada por el fuego, generando pérdidas de vidas humanas, animales y bienes materiales.',
+    },
+    {
+      termino: 'Posible',
+      significado:
+        'amenaza que, según el diagnóstico de sus condiciones inherentes, no ha sucedido, pero puede llegar a ocurrir.',
+    },
+    {
+      termino: 'Primeros auxilios',
+      significado:
+        'son aquellas primeras medidas o cuidados adecuados que se ponen en práctica en forma provisional tan pronto se reconoce una emergencia.',
+    },
+    {
+      termino: 'Recurso',
+      significado:
+        'son las personas, elementos, equipos y herramientas con los cuales se cuenta para evitar un desastre y atender correctamente la situación en peligro.',
+    },
+    {
+      termino: 'Riesgo',
+      significado:
+        'probabilidad de que un objeto, sujeto, materia, sustancia o fenómeno pueda perturbar la integridad física de la persona o el medio.',
+    },
+    {
+      termino: 'Testigo',
+      significado:
+        'persona que presencia o adquiere directo y verdadero conocimiento de algo.',
     },
   ],
   referencias: [
+    {
+      referencia: '',
+      link: '',
+    },
+    {
+      referencia: '',
+      link: '',
+    },
+    {
+      referencia: '',
+      link: '',
+    },
+    {
+      referencia: '',
+      link: '',
+    },
     {
       referencia: '',
       link: '',
